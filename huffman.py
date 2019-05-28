8f2b243d74432079e96c9bcd33f9a1b46348f6b4
mark-koren <mkorenfhspltw09@gmail.com>
2019-05-27 23:21:47 -0700

    updated huffman

--- huffman.py
+++ huffman.py
@@ -9,181 +9,211 @@ Explanation at http://bhrigu.me/blog/2017/01/17/huffman-coding-python-implementa
 
 @total_ordering
 class HeapNode:
-	def __init__(self, char, freq):
-		self.char = char
-		self.freq = freq
-		self.left = None
-		self.right = None
+    def __init__(self, char, freq):
+        self.char = char
+        self.freq = freq
+        self.left = None
+        self.right = None
 
-	# defining comparators less_than and equals
-	def __lt__(self, other):
-		return self.freq < other.freq
+    # defining comparators less_than and equals
+    def __lt__(self, other):
+        return self.freq < other.freq
 
-	def __eq__(self, other):
-		if(other == None):
-			return False
-		if(not isinstance(other, HeapNode)):
-			return False
-		return self.freq == other.freq
+    def __eq__(self, other):
+        if(other == None):
+            return False
+        if(not isinstance(other, HeapNode)):
+            return False
+        return self.freq == other.freq
 
 
 class HuffmanCoding:
-	def __init__(self, path):
-		self.path = path
-		self.heap = []
-		self.codes = {}
-		self.reverse_mapping = {}
+    def __init__(self, path):
+        self.path = path
+        self.heap = []
+        self.codes = {}
+        self.reverse_mapping = {}
 
-	# functions for compression:
+    # functions for compression:
 
-	def make_frequency_dict(self, text):
-		frequency = {}
-		for character in text:
-			if not character in frequency:
-				frequency[character] = 0
-			frequency[character] += 1
-		return frequency
+    def make_frequency_dict(self, text):
+        frequency = {}
+        for character in text:
+            if not character in frequency:
+                frequency[character] = 0
+            frequency[character] += 1
+        return frequency
 
-	def make_heap(self, frequency):
-		for key in frequency:
-			node = HeapNode(key, frequency[key])
-			heapq.heappush(self.heap, node)
+    def make_heap(self, frequency):
+        for key in frequency:
+            node = HeapNode(key, frequency[key])
+            heapq.heappush(self.heap, node)
 
-	def merge_nodes(self):
-		while(len(self.heap)>1):
-			node1 = heapq.heappop(self.heap)
-			node2 = heapq.heappop(self.heap)
+    def merge_nodes(self):
+        while(len(self.heap)>1):
+            node1 = heapq.heappop(self.heap)
+            node2 = heapq.heappop(self.heap)
 
-			merged = HeapNode(None, node1.freq + node2.freq)
-			merged.left = node1
-			merged.right = node2
+            merged = HeapNode(None, node1.freq + node2.freq)
+            merged.left = node1
+            merged.right = node2
 
-			heapq.heappush(self.heap, merged)
-
-
-	def make_codes_helper(self, root, current_code):
-		if(root == None):
-			return
-
-		if(root.char != None):
-			self.codes[root.char] = current_code
-			self.reverse_mapping[current_code] = root.char
-			return
-
-		self.make_codes_helper(root.left, current_code + "0")
-		self.make_codes_helper(root.right, current_code + "1")
+            heapq.heappush(self.heap, merged)
 
 
-	def make_codes(self):
-		root = heapq.heappop(self.heap)
-		current_code = ""
-		self.make_codes_helper(root, current_code)
+    def make_codes_helper(self, root, current_code):
+        if(root == None):
+            return
+
+        if(root.char != None):
+            self.codes[root.char] = current_code
+            self.reverse_mapping[current_code] = root.char
+            return
+
+        self.make_codes_helper(root.left, current_code + "0")
+        self.make_codes_helper(root.right, current_code + "1")
 
 
-	def get_encoded_text(self, text):
-		encoded_text = ""
-		for character in text:
-			encoded_text += self.codes[character]
-		return encoded_text
+    def make_codes(self):
+        root = heapq.heappop(self.heap)
+        current_code = ""
+        self.make_codes_helper(root, current_code)
 
 
-	def pad_encoded_text(self, encoded_text):
-		extra_padding = 8 - len(encoded_text) % 8
-		for i in range(extra_padding):
-			encoded_text += "0"
+    def get_encoded_text(self, text):
+        encoded_text = ""
+        for character in text:
+            encoded_text += self.codes[character]
+        return encoded_text
 
-		padded_info = "{0:08b}".format(extra_padding)
-		encoded_text = padded_info + encoded_text
-		return encoded_text
+    def get_encoded_array(self,data):
+        encoded_array = []
+        for character in data:
+            encoded_array.append(self.codes[character])
+        return encoded_array
 
+    def pad_encoded_text(self, encoded_text):
+        extra_padding = 8 - len(encoded_text) % 8
+        for i in range(extra_padding):
+            encoded_text += "0"
 
-	def get_byte_array(self, padded_encoded_text):
-		if(len(padded_encoded_text) % 8 != 0):
-			print("Encoded text not padded properly")
-			exit(0)
-
-		b = bytearray()
-		for i in range(0, len(padded_encoded_text), 8):
-			byte = padded_encoded_text[i:i+8]
-			b.append(int(byte, 2))
-		return b
-
-	def create_coding(self):
-		filename, file_extension = os.path.splitext(self.path)
-		output_path = filename + ".bin"
-
-		with open(self.path, 'r+') as file:
-			text = file.read()
-			text = text.rstrip()
-
-			frequency = self.make_frequency_dict(text)
-			self.make_heap(frequency)
-			self.merge_nodes()
-			self.make_codes()
-
-	def compress(self):
-		filename, file_extension = os.path.splitext(self.path)
-		output_path = filename + ".bin"
-		with open(self.path, 'r+') as file, open(output_path, 'wb') as output:
-			text = file.read()
-			text = text.rstrip()
-
-			encoded_text = self.get_encoded_text(text)
-			padded_encoded_text = self.pad_encoded_text(encoded_text)
-
-			b = self.get_byte_array(padded_encoded_text)
-			output.write(bytes(b))
-
-		print("Compressed")
-		return output_path
+        padded_info = "{0:08b}".format(extra_padding)
+        encoded_text = padded_info + encoded_text
+        return encoded_text
 
 
-	""" functions for decompression: """
+    def get_byte_array(self, padded_encoded_text):
+        if(len(padded_encoded_text) % 8 != 0):
+            print("Encoded text not padded properly")
+            exit(0)
+
+        b = bytearray()
+        for i in range(0, len(padded_encoded_text), 8):
+            byte = padded_encoded_text[i:i+8]
+            b.append(int(byte, 2))
+        return b
+
+    def create_coding(self):
+        filename, file_extension = os.path.splitext(self.path)
+        output_path = filename + ".bin"
+
+        with open(self.path, 'r+') as file:
+            text = file.read()
+            text = text.rstrip()
+
+            frequency = self.make_frequency_dict(text)
+            self.make_heap(frequency)
+            self.merge_nodes()
+            self.make_codes()
+
+    def create_coding_from_binary(self, data):
+        frequency = self.make_frequency_dict(data)
+        self.make_heap(frequency)
+        self.merge_nodes()
+        self.make_codes()
+
+    def compress_from_binary(self, data):
+        filename, file_extension = os.path.splitext(self.path)
+        output_path = filename + "_encoded.p"
+        with open(output_path, 'wb') as output:
+            encoded_array = self.get_encoded_array(data)
+            pickle.dump(encoded_array, output)
+
+            print("Compressed")
+        return output_path
+
+    def compress(self):
+        filename, file_extension = os.path.splitext(self.path)
+        output_path = filename + ".bin"
+        with open(self.path, 'r+') as file, open(output_path, 'wb') as output:
+            text = file.read()
+            text = text.rstrip()
+
+            encoded_text = self.get_encoded_text(text)
+            padded_encoded_text = self.pad_encoded_text(encoded_text)
+
+            b = self.get_byte_array(padded_encoded_text)
+            output.write(bytes(b))
+
+        print("Compressed")
+        return output_path
 
 
-	def remove_padding(self, padded_encoded_text):
-		padded_info = padded_encoded_text[:8]
-		extra_padding = int(padded_info, 2)
-
-		padded_encoded_text = padded_encoded_text[8:] 
-		encoded_text = padded_encoded_text[:-1*extra_padding]
-
-		return encoded_text
-
-	def decode_text(self, encoded_text):
-		current_code = ""
-		decoded_text = ""
-
-		for bit in encoded_text:
-			current_code += bit
-			if(current_code in self.reverse_mapping):
-				character = self.reverse_mapping[current_code]
-				decoded_text += character
-				current_code = ""
-
-		return decoded_text
+    """ functions for decompression: """
 
 
-	def decompress(self, input_path):
-		filename, file_extension = os.path.splitext(self.path)
-		output_path = filename + "_decompressed" + ".txt"
+    def remove_padding(self, padded_encoded_text):
+        padded_info = padded_encoded_text[:8]
+        extra_padding = int(padded_info, 2)
 
-		with open(input_path, 'rb') as file, open(output_path, 'w') as output:
-			bit_string = ""
+        padded_encoded_text = padded_encoded_text[8:] 
+        encoded_text = padded_encoded_text[:-1*extra_padding]
 
-			byte = file.read(1)
-			while(len(byte) > 0):
-				byte = ord(byte)
-				bits = bin(byte)[2:].rjust(8, '0')
-				bit_string += bits
-				byte = file.read(1)
+        return encoded_text
 
-			encoded_text = self.remove_padding(bit_string)
+    def decode_text(self, encoded_text):
+        current_code = ""
+        decoded_text = ""
 
-			decompressed_text = self.decode_text(encoded_text)
-			
-			output.write(decompressed_text)
+        for bit in encoded_text:
+            current_code += bit
+            if(current_code in self.reverse_mapping):
+                character = self.reverse_mapping[current_code]
+                decoded_text += character
+                current_code = ""
 
-		print("Decompressed")
-		return output_path
+        return decoded_text
+
+
+    def decompress(self, input_path):
+        filename, file_extension = os.path.splitext(self.path)
+        output_path = filename + "_decompressed" + ".txt"
+
+        with open(input_path, 'rb') as file, open(output_path, 'w') as output:
+            bit_string = ""
+
+            byte = file.read(1)
+            while(len(byte) > 0):
+                byte = ord(byte)
+                bits = bin(byte)[2:].rjust(8, '0')
+                bit_string += bits
+                byte = file.read(1)
+
+            encoded_text = self.remove_padding(bit_string)
+
+            decompressed_text = self.decode_text(encoded_text)
+            
+            output.write(decompressed_text)
+
+        print("Decompressed")
+        return output_path
+
+    def decompress_from_binary(self):
+        filename, file_extension = os.path.splitext(self.path)
+        output_path = filename + "_encoded.p"
+        with open(output_path, 'rb') as encoded_f:
+            encoded_array = pickle.load(encoded_f)
+            decoded_array = []
+            
+
 
--- useHuffman.py
+++ useHuffman.py
@@ -1,18 +1,34 @@
 from huffman import HuffmanCoding
 import sys
 import time
+import pickle
+import numpy as np
+import pdb
 
 path = "sample.txt"
 
-h = HuffmanCoding(path)
+# h = HuffmanCoding(path)
+with open('./DJIEncoded.p', 'rb') as f: 
+    data = pickle.load(f)
+    parsed_data=[]
+    for i in range(data.shape[0]//10):
+        print("Converting encoded image ", i)
+        for j in range(data.shape[1]//16):
+            text_array = ""
+            for k in range(16):
+                text_array += str(data[i,16*j+k])
+            parsed_data.append(text_array)
 
-start = time.time()
-h.create_coding()
-print("Coding created in ", time.time() - start, " seconds")
-start = time.time()
-output_path = h.compress()
-print("Compressed in ", time.time() - start, " seconds")
-print("Compressed file path: " + output_path)
+    pdb.set_trace()
 
-decom_path = h.decompress(output_path)
-print("Decompressed file path: " + decom_path)
+    h = HuffmanCoding(None)
+    start = time.time()
+    h.create_coding_from_binary(parsed_data)
+    print("Coding created in ", time.time() - start, " seconds")
+    start = time.time()
+    output_path = h.compress_from_binary()
+    print("Compressed in ", time.time() - start, " seconds")
+    print("Compressed file path: " + output_path)
+
+    decom_path = h.decompress(output_path)
+    print("Decompressed file path: " + decom_path)
