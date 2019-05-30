dde8d0816c7f45f5e263804542bd57786b37c50d
Mark Koren <mkoren@stanford.edu>
2019-05-29 17:09:10 -0700

    running over multiple clusters

--- useHuffman.py
+++ useHuffman.py
@@ -17,7 +17,7 @@ with open('./DJIEncoded.p', 'rb') as f:
     parsed_data=[]
     freq_count = np.zeros((data.shape[0],2**16))
     # pdb.set_trace()
-    for i in range(data.shape[0]//10):
+    for i in range(data.shape[0]):
         print("Converting encoded image ", i)
         for j in range(data.shape[1]//16):
             text_array = ""
@@ -30,24 +30,91 @@ with open('./DJIEncoded.p', 'rb') as f:
 
     parsed_array = np.array(parsed_data)
 
-    num_clusters = 16
-    kmeans = kmeans = KMeans(n_clusters=num_clusters, random_state=0, max_iter=1000, verbose=1, n_init=1).fit(freq_count[0:data_train])
+    cluster_array = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+    size_array = np.zeros(len(cluster_array))
+    for j in range(len(cluster_array)):
+	    num_clusters = cluster_array[j]
+	    kmeans = kmeans = KMeans(n_clusters=num_clusters, random_state=0, max_iter=1000, verbose=1, n_init=1).fit(freq_count[0:data_train])
+	    labels = kmeans.predict(freq_count)
+	    # pdb.set_trace()
+	    cluster_sizes = np.zeros(num_clusters)
+	    for cluster in range(num_clusters):
+	        # a0 = np.argwhere(kmeans.labels_ == 0).squeeze().tolist()
+	        # print("cluster ", cluster, ": ", np.argwhere(kmeans.labels_ == cluster).squeeze().tolist())
+	        cluster_data = []
+	        for i in np.argwhere(labels == cluster).reshape(-1).tolist():
+	        	cluster_data += parsed_data[i*(data.shape[1]//16):(i+1)*(data.shape[1]//16)]
 
+	        # cluster_data = [parsed_data[i*(data.shape[1]//16):(i+1)*(data.shape[1]//16)] for i in np.argwhere(labels == cluster).reshape(-1).tolist()]
+	        # cluster_data = parsed_array(np.argwhere(kmeans.labels_ == cluster))
+	        # pdb.set_trace()
+	        h = HuffmanCoding('DJIEncoded.txt')
+	        start = time.time()
+	        h.create_coding_from_binary(cluster_data)
+	        print("Coding created in ", time.time() - start, " seconds")
+	        start = time.time()
+	        encoded_array, size = h.get_encoded_array(cluster_data)
+	        cluster_sizes[cluster] = size
+
+	    print("Clustered: ", np.sum(cluster_sizes))
+	    size_array[j] = np.sum(cluster_sizes) + data.shape[0] * np.log2(num_clusters)
+
+    cluster_array = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+    size_array = np.zeros(len(cluster_array))
+    for j in range(len(cluster_array)):
+	    num_clusters = cluster_array[j]
+	    kmeans = kmeans = KMeans(n_clusters=num_clusters, random_state=0, max_iter=1000, verbose=1, n_init=1).fit(freq_count[0:data_train])
+	    labels = kmeans.predict(freq_count)
+	    # pdb.set_trace()
+	    cluster_sizes = np.zeros(num_clusters)
+	    for cluster in range(num_clusters):
+	        # a0 = np.argwhere(kmeans.labels_ == 0).squeeze().tolist()
+	        # print("cluster ", cluster, ": ", np.argwhere(kmeans.labels_ == cluster).squeeze().tolist())
+	        cluster_data = []
+	        for i in np.argwhere(labels == cluster).reshape(-1).tolist():
+	        	cluster_data += parsed_data[i*(data.shape[1]//16):(i+1)*(data.shape[1]//16)]
+
+	        # cluster_data = [parsed_data[i*(data.shape[1]//16):(i+1)*(data.shape[1]//16)] for i in np.argwhere(labels == cluster).reshape(-1).tolist()]
+	        # cluster_data = parsed_array(np.argwhere(kmeans.labels_ == cluster))
+	        # pdb.set_trace()
+	        h = HuffmanCoding('DJIEncoded.txt')
+	        start = time.time()
+	        h.create_coding_from_binary(cluster_data)
+	        print("Coding created in ", time.time() - start, " seconds")
+	        start = time.time()
+	        encoded_array, size = h.get_encoded_array(cluster_data)
+	        cluster_sizes[cluster] = size
+
+	    print("Clustered: ", np.sum(cluster_sizes))
+	    size_array[j] = np.sum(cluster_sizes) + data.shape[0] * np.log2(num_clusters)
+
+    # cluster_sizes = np.zeros(num_clusters)
+    # for cluster in range(num_clusters):
+    #     # a0 = np.argwhere(kmeans.labels_ == 0).squeeze().tolist()
+    #     # print("cluster ", cluster, ": ", np.argwhere(kmeans.labels_ == cluster).squeeze().tolist())
+    #     cluster_data = [parsed_data[i+data_test] for i in np.argwhere(labels[data_test:-1] == cluster).reshape(-1).tolist()]
+    #     # cluster_data = parsed_array(np.argwhere(kmeans.labels_ == cluster))
+    #     h = HuffmanCoding('DJIEncoded.txt')
+    #     start = time.time()
+    #     h.create_coding_from_binary(cluster_data)
+    #     print("Coding created in ", time.time() - start, " seconds")
+    #     start = time.time()
+    #     encoded_array, size = h.get_encoded_array(cluster_data)
+    #     cluster_sizes[cluster] = size
+
+    # print("Clustered (Test only): ", np.sum(cluster_sizes))
     # pdb.set_trace()
-    cluster_sizes = np.zeros(num_clusters)
-    for cluster in range(num_clusters):
-        # a0 = np.argwhere(kmeans.labels_ == 0).squeeze().tolist()
-        print("cluster ", cluster, ": ", np.argwhere(kmeans.labels_ == cluster).squeeze().tolist())
-        cluster_data = [parsed_data[i] for i in np.argwhere(kmeans.labels_ == cluster).squeeze().tolist()]
-        # cluster_data = parsed_array(np.argwhere(kmeans.labels_ == cluster))
-        h = HuffmanCoding('DJIEncoded.txt')
-        start = time.time()
-        h.create_coding_from_binary(cluster_data)
-        print("Coding created in ", time.time() - start, " seconds")
-        start = time.time()
-        encoded_array, size = h.get_encoded_array(cluster_data)
-        cluster_sizes[cluster] = size
 
+    # h = HuffmanCoding('DJIEncoded.txt')
+    # start = time.time()
+    # h.create_coding_from_binary(parsed_data)
+    # print("Coding created in ", time.time() - start, " seconds")
+    # start = time.time()
+    # encoded_array, size = h.get_encoded_array(parsed_data)
+    # cluster_sizes[cluster] = size
+
+    # print("Total: ", size)
+    print(size_array)
     pdb.set_trace()
         # output_path, size = h.compress_from_binary(data)
         # print("Compressed in ", time.time() - start, " seconds")
